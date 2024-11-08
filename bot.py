c17f36a8d4d7188d4b2d24749baa39b80affa727
Marco Balducci <balduccimarco2007@gmail.com>
2024-11-08 18:36:25 +0100

    added newline in bot.py

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ async def register(interaction: discord.Interaction, class_year_and_section: str
 
     if instances.get(interaction.guild_id) is None:
         instances[interaction.guild_id] = server.server(guild_id=interaction.guild_id, channel=interaction.channel, class_identifier=class_year_and_section)
-        open("./servers.txt", 'a').write(f'{interaction.guild_id}#{interaction.channel.id}#{class_year_and_section}')
+        open("./servers.txt", 'a').write(f'{interaction.guild_id}#{interaction.channel.id}#{class_year_and_section}\n')
         await interaction.response.send_message("Server added to update list!", ephemeral=True)
     else:
         await interaction.response.send_message("The server is already registered, there is no need to register it again.", ephemeral=True)
